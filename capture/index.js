f4dff9a576c08c8a762a0f922fe1c1c59e17fbb0
Kevin Albertson <kevin.albertson@10gen.com>
2017-10-31 12:17:36 -0400

    Hopefully fix capture websocket code

--- capture/index.js
+++ capture/index.js
@@ -1,30 +1,29 @@
 var app = require('express')();
 var http = require('http').Server(app);
-var io = require('socket.io')(http);
+let WebSocketServer = require('uws').Server;
+let wss = new WebSocketServer({ port: 3000 });
 
 app.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html');
 });
 
-io.on('connection', function(socket){
+wss.on('connection', function(socket){
 	console.log('a user connected');
 
 
 	socket.on('disconnect', function(){
 		console.log('user disconnected');
 	});
-	socket.on('message', function(message){
+	socket.on('message', function(arrayBuffer){
+		var message = new Uint8Array(arrayBuffer);
+
 		var x = message[0] + (message[1] << 8);
 		var y = message[2] + (message[3] << 8);
 
-		var msg = { mes: [socket.id, x, y] };
+		var msg = { mes: [ x, y] };
 		// buffer.push(msg);
 		// if (buffer.length > 15) buffer.shift();
 		// client.broadcast(msg);
 		console.log(msg);
 	});
-});
-
-http.listen(3333, function(){
-  console.log('listening on *:3333');
 });
